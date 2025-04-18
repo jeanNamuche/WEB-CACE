687df6d1ec838a5a261731998321ca70f065f5ae
Jean Namuche <jean09namuche@gmail.com>
2025-04-17 23:47:43 -0500

    Primer commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+    <title>CACECOB</title>
+      <h1 class="text-3xl font-bold underline">
+       Hello world!
+      </h1>
+
+      <h2 class="text-2xl font-semibold">esto es una prueba</h2>
+      
+    </>
+  )
+}
+
+export default App
